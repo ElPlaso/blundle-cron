1e74b62d0e7fa78f43cb65c96e48abdcc1b345bb
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2024-11-03 23:43:34 +1300

    setup redis client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World')
+import 'dotenv/config';
+import Redis from "ioredis";
+
+const { REDIS_URL } = process.env;
+
+if (!REDIS_URL) {
+    throw new Error('Redis URL not provided');
+}
+
+try {
+    const renderRedis = new Redis(REDIS_URL);
+
+    console.log(REDIS_URL);
+}
+catch (error) {
+    console.error(error);
+}
+
+
